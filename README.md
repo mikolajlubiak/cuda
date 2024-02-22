dc364560cb938c2b4a3630680a798de8313317f2
Mikołaj Lubiak <lubiak@proton.me>
2024-02-22 15:25:59 +0100

    Generate the vector using GPU, allow SIZE to be above 1024

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # cuda
 Just me playing around with CUDA.
+
 Results:
 ```
-Elapsed CPU: 2.985657 seconds
-Elapsed GPU: 0.000539 seconds
+❯ ./a.out
+Elapsed CPU: 0.593751 seconds
+Elapsed GPU: 0.002913 seconds
+199999998, 199999998
 ```
--- main.cu
+++ main.cu
@@ -8,8 +8,8 @@
 #include <time.h>
 
 void add_vectors_cpu(const int *const a, const int *const b, int *const c,
-                     const int size) {
-  for (int i = 0; i < size; i++) {
+                     const int num) {
+  for (int i = 0; i < num; i++) {
     c[i] = a[i] + b[i];
   }
 }
@@ -20,65 +20,68 @@ __global__ void add_vectors_gpu(const int *const a, const int *const b,
   c[i] = a[i] + b[i];
 }
 
-int *createdata(int num) {
-  int *ptr;
-
-  ptr = (int *)malloc(sizeof(int) * num);
-
-  if (ptr != NULL) {
-    for (int i = 0; i < num; i++) {
-      ptr[i] = 0;
-    }
+void gendata_cpu(int *const arr, const int num) {
+  for (int i = 0; i < num; i++) {
+    arr[i] = i;
   }
-  return ptr;
 }
 
-int *gendata(int num) {
-  int *ptr = createdata(num);
-
-  if (ptr != NULL) {
-    for (int j = 0; j < num; j++) {
-      ptr[j] = rand();
-    }
-  }
-  return ptr;
+__global__ void gendata_gpu(int *const arr) {
+  int i = threadIdx.x;
+  arr[i] = i;
 }
 
 int main() {
   clock_t tic;
   clock_t toc;
-  const int SIZE = 1000000000; // 1_000_000_000
+  const int SIZE = 100000000; // 100_000_000
+  const int MEM_SIZE = SIZE * sizeof(int);
+  const int BLOCK_SIZE = 256;
+  const int GRID_SIZE = (SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-  int *a = gendata(SIZE);
-  int *b = gendata(SIZE);
-  int *c = (int *)malloc(SIZE * sizeof(int));
+  int *a = (int *)malloc(MEM_SIZE);
+  int *b = (int *)malloc(MEM_SIZE);
+  int *c = (int *)malloc(MEM_SIZE);
 
   int *cuda_a;
   int *cuda_b;
   int *cuda_c;
+  cudaMalloc(&cuda_a, MEM_SIZE);
+  cudaMalloc(&cuda_b, MEM_SIZE);
+  cudaMalloc(&cuda_c, MEM_SIZE);
 
-  cudaMalloc(&cuda_a, SIZE * sizeof(int));
-  cudaMalloc(&cuda_b, SIZE * sizeof(int));
-  cudaMalloc(&cuda_c, SIZE * sizeof(int));
-
-  cudaMemcpy(cuda_a, a, SIZE * sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(cuda_b, b, SIZE * sizeof(int), cudaMemcpyHostToDevice);
-
+  // CPU
   tic = clock();
+
+  gendata_cpu(a, SIZE);
+  gendata_cpu(b, SIZE);
+
   add_vectors_cpu(a, b, c, SIZE);
+
   toc = clock();
+
   printf("Elapsed CPU: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
-  int test_cpu = c[5];
+  int test_cpu = c[SIZE - 1];
 
+  // GPU
   tic = clock();
-  add_vectors_gpu<<<1, SIZE>>>(cuda_a, cuda_b, cuda_c);
+
+  gendata_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(a);
+  gendata_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(b);
+
+  cudaMemcpy(cuda_a, a, MEM_SIZE, cudaMemcpyHostToDevice);
+  cudaMemcpy(cuda_b, b, MEM_SIZE, cudaMemcpyHostToDevice);
+
+  add_vectors_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(cuda_a, cuda_b, cuda_c);
+
   toc = clock();
+
   printf("Elapsed GPU: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+  cudaMemcpy(c, cuda_c, MEM_SIZE, cudaMemcpyDeviceToHost);
+  int test_gpu = c[SIZE - 1];
 
-  cudaMemcpy(c, cuda_c, SIZE * sizeof(int), cudaMemcpyDeviceToHost);
-  int test_gpu = c[5];
-
-  assert(test_gpu == test_cpu);
+  printf("%d, %d\n", test_cpu, test_gpu);
+  assert(test_cpu == test_gpu);
 
   return 0;
 }
