9ce1e6be39839d2e753b3bc66b9343b7e17f2ac6
Mikołaj Lubiak <lubiak@proton.me>
2024-02-22 17:08:11 +0100

    Avoid unnecessary `cudaMemcpy`

--- main.cu
+++ main.cu
@@ -14,21 +14,25 @@ void add_vectors_cpu(const int *const a, const int *const b, int *const c,
   }
 }
 
-__global__ void add_vectors_gpu(const int *const a, const int *const b,
-                                int *const c) {
-  int i = threadIdx.x;
-  c[i] = a[i] + b[i];
-}
-
 void gendata_cpu(int *const arr, const int num) {
   for (int i = 0; i < num; i++) {
     arr[i] = i;
   }
 }
 
-__global__ void gendata_gpu(int *const arr) {
-  int i = threadIdx.x;
-  arr[i] = i;
+__global__ void add_vectors_gpu(const int *const a, const int *const b,
+                                int *const c, const int num) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  if (i < num) {
+    c[i] = a[i] + b[i];
+  }
+}
+
+__global__ void gendata_gpu(int *const arr, const int num) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  if (i < num) {
+    arr[i] = i;
+  }
 }
 
 int main() {
@@ -36,7 +40,10 @@ int main() {
   clock_t toc;
   const int SIZE = 100000000; // 100_000_000
   const int MEM_SIZE = SIZE * sizeof(int);
-  const int BLOCK_SIZE = 256;
+  cudaDeviceProp deviceProp;
+  cudaGetDeviceProperties(&deviceProp, 0);
+  const int MAX_THREADS_PER_BLOCK = deviceProp.maxThreadsPerBlock;
+  const int BLOCK_SIZE = min(MAX_THREADS_PER_BLOCK, SIZE);
   const int GRID_SIZE = (SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
   int *a = (int *)malloc(MEM_SIZE);
@@ -61,22 +68,21 @@ int main() {
   toc = clock();
 
   printf("Elapsed CPU: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+
   int test_cpu = c[SIZE - 1];
 
   // GPU
   tic = clock();
 
-  gendata_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(a);
-  gendata_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(b);
+  gendata_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(cuda_a, SIZE);
+  gendata_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(cuda_b, SIZE);
 
-  cudaMemcpy(cuda_a, a, MEM_SIZE, cudaMemcpyHostToDevice);
-  cudaMemcpy(cuda_b, b, MEM_SIZE, cudaMemcpyHostToDevice);
-
-  add_vectors_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(cuda_a, cuda_b, cuda_c);
+  add_vectors_gpu<<<GRID_SIZE, BLOCK_SIZE>>>(cuda_a, cuda_b, cuda_c, SIZE);
 
   toc = clock();
 
   printf("Elapsed GPU: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+
   cudaMemcpy(c, cuda_c, MEM_SIZE, cudaMemcpyDeviceToHost);
   int test_gpu = c[SIZE - 1];
 
